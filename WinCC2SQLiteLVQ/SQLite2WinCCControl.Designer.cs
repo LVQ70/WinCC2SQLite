fda3e6648b1fc6eab4a98bf92b1203256229e125
鹿其 <oncwnuAatZPrsEzGAjz614a2G_q0@git.weixin.qq.com>
2024-02-20 14:49:12 +0800

    首次提交

---
+++ WinCC2SQLiteLVQ/SQLite2WinCCControl.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace WinCC2SQLiteLVQ
+{
+    partial class SQLite2WinCCControl
+    {
+        /// <summary> 
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary> 
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ChooseTable = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ChooseTable
+            // 
+            this.ChooseTable.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ChooseTable.Location = new System.Drawing.Point(3, 3);
+            this.ChooseTable.Name = "ChooseTable";
+            this.ChooseTable.Size = new System.Drawing.Size(90, 32);
+            this.ChooseTable.TabIndex = 6;
+            this.ChooseTable.Text = "选择";
+            this.ChooseTable.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 30;
+            this.dataGridView1.Size = new System.Drawing.Size(150, 150);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // SQLite2WinCCControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.ChooseTable);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "SQLite2WinCCControl";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ChooseTable;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ WinCC2SQLiteLVQ/SQLite2WinCCControl.cs
@@ -0,0 +1,53 @@
+﻿using CCHMIRUNTIME;
+using System;
+using System.Data;
+using System.Data.SQLite;
+using System.Windows.Forms;
+
+namespace WinCC2SQLiteLVQ
+{
+    public partial class SQLite2WinCCControl : UserControl
+    {
+
+        public string HT_SQLitePath { get; set; } //数据库存放地址
+        public string HT_SQL { get; set; } // SQL语句
+        public string HT_Tag2WinCC { get; set; } // 存入WinCC数据点
+
+        public SQLite2WinCCControl()
+        {
+            InitializeComponent();
+        }
+
+        public void initSQLiteDB()
+        {
+            string conStr = "data source= " + HT_SQLitePath + ";version=3;";
+
+            using (SQLiteConnection connect = new SQLiteConnection(conStr))
+            {
+                //打开文件
+                connect.Open();
+                DataTable dt = new DataTable();
+                SQLiteDataAdapter sqliteAdapter = new SQLiteDataAdapter(HT_SQL, connect);
+                sqliteAdapter.Fill(dt);//结果填充到DataTable中
+                this.dataGridView1.DataSource = dt;
+            }
+        }
+
+        private void ChooseTable_Click(object sender, EventArgs e)
+        {
+            HMIRuntime cac = new HMIRuntime();
+            for (int i = 0; i < dataGridView1.Rows.Count; i++)
+            {
+                if (dataGridView1.Rows[i].Selected == true)
+                {
+                    cac.Tags[HT_Tag2WinCC].Write(dataGridView1.Rows[i].Cells[0].Value.ToString());
+                }
+            }
+        }
+
+        private void HT_SQLite2WinCC_Load(object sender, EventArgs e)
+        {
+            initSQLiteDB();
+        }
+    }
+}
